25afdbb6c10163d963d2f430f59324741c1e54db
arkrishna <arkrishna2022@gmail.com>
2025-07-16 21:07:35 +0530

    workflow-fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express')
 const app=express()
 
 app.get('/',(req,res)=>{
+    console.log('CI/CD pipeline is working!...log added!')
     res.send('CI/CD pipeline is working!')
 })
 
